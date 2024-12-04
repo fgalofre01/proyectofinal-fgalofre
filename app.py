3f4ee8566e1bafdf57ab86cf4d5792e825afa060
fgalofre01 <fabiangalofre@gmail.com>
2024-12-04 00:39:05 -0500

    Instalacion de algunas dependencias

--- app.py
+++ app.py
@@ -4,10 +4,13 @@ from utils.db import db,init_db
 from views.Heladeria_controller import heladeria_routes
 from views.API import heladeria_apis
 from flask_restful import Api
+import pymysql
 import os
+
 
 load_dotenv(override=True)
 app = Flask(__name__)
+pymysql.install_as_MySQLdb()
 
 secret_key = os.urandom(24)
 app.config['SECRET_KEY'] = secret_key
